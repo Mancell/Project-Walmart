90a0a72fbc450720cc78061dc43227ae2c340bcc
Mahmut Ali Öncel <moncel1091@gmail.com>
2024-12-02 22:49:38 +0300

    vfvf

--- walmart.sql
+++ walmart.sql
@@ -4,9 +4,27 @@
 -- Business Problems
 --Q.1 Find different payment method and number of transactions, number of qty sold
 
-SELECT 
+/*SELECT 
 	 payment_method,
 	 COUNT(*) as no_payments,
 	 SUM(quantity) as no_qty_sold
 FROM walmart
 GROUP BY payment_method
+*/
+
+
+-- Project Question #2
+-- Identify the highest-rated category in each branch, displaying the branch, category
+-- AVG RATING
+
+SELECT * 
+FROM
+(	SELECT 
+		branch,
+		category,
+		AVG(rating) as avg_rating,
+		RANK() OVER(PARTITION BY branch ORDER BY AVG(rating) DESC) as rank
+	FROM walmart
+	GROUP BY 1, 2
+)
+WHERE rank = 1
