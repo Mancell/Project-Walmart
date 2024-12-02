b318be3f0aeee34a2da89b180a8ae3aeba9e97c6
Mahmut Ali Öncel <moncel1091@gmail.com>
2024-12-02 22:39:50 +0300

    dfdf

--- walmart.sql
+++ walmart.sql
@@ -1,1 +1,12 @@
-select * from walmart
+
+/*select * from walmart*/
+
+-- Business Problems
+--Q.1 Find different payment method and number of transactions, number of qty sold
+
+SELECT 
+	 payment_method,
+	 COUNT(*) as no_payments,
+	 SUM(quantity) as no_qty_sold
+FROM walmart
+GROUP BY payment_method
